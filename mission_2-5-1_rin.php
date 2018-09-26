818ce51fcb7501b78d6c686cf524154bbd35e2f1
wimpykidlsy <43592737+wimpykidlsy@users.noreply.github.com>
2018-09-26 10:43:51 +0900

    Add files via upload

---
+++ mission_2-5-1_rin.php
@@ -0,0 +1,172 @@
+﻿ <?php
+   //変数の定義
+  $name = "名前";
+  $comment = "コメント";
+  $password = "パスワード";
+  $datafile = 'mission_2-5-1_rin.txt';
+  $fpa = fopen($datafile,'a');
+  $file = file($datafile);
+  $numberfile = 'mission_2-5-1_number_rin.txt';
+  $fpnw_d = fopen($numberfile,'w');
+  $numberfile_e = 'mission_2-5-1_number_e_rin.txt';
+  $fpnw_e = fopen($numberfile_e,'w');
+  $numberfile_n = 'mission_2-5-1_number_n_rin.txt';
+  $fpnw_n = fopen($numberfile_n,'w');
+  $date = date('Y/m/d/ H:i:s');
+  $data_name = $_POST['name'];
+  $data_comment = $_POST['comment'];
+  $data_password = $_POST['password'];
+  $data_password_d = $_POST['password_d'];
+  $data_password_e = $_POST['password_e'];
+  $editpassword = $_POST['passwordedit'];
+  $data_number_d = $_POST['number_delete'];
+  $data_number_e = $_POST['number_edit'];
+  $data_number_n = $_POST['number_e'];
+
+   //編集番号をファイルへ書き込む
+  if(!empty($data_number_e)){
+   $data_e = $data_number_e."<>".$data_password_e."<>";
+   fwrite($fpnw_e,$data_e);
+   fclose($fpnw_e);
+  }
+   //編集番号指定の行の名前とコメントを変数に代入
+  if(!empty($data_number_e)){
+   $contents = file('mission_2-5-1_rin.txt');  //ファイルを配列として
+   $edits = file('mission_2-5-1_number_e_rin.txt');  //指定番号
+   $edit = explode("<>",$edits[0]);
+   $editnumber = $edit[0];
+   $outnumber = $edit[0] - 1;
+   $passworde = $edit[1];
+   for($a = 0;$a < count($file);$a++){
+    $newline = explode("<>",$file[$a]);
+    $newpwd = $newline[4];
+    $nmb = $newline[0];
+    if(($editnumber == $nmb)&&($passworde == $newpwd)){  //二つの指定番号とパスワードが一致するとき
+     $name = $newline[1];
+     $comment = $newline[2];
+     $password = $newline[4];
+    }else if($editnumber == $nmb){
+     echo "パスワードが違います。";
+    }
+   }
+  }
+   //内容を削除
+  if(!empty($data_number_d) && ($data_password_d)){
+   $data_d = $data_number_d."<>".$data_password_d."<>";
+   fwrite($fpnw_d,$data_d);
+   fclose($fpnw_d);
+  }
+  if(!empty($data_number_d) && ($data_password_d)){
+   $contents = file('mission_2-5-1_rin.txt');  //入力ファイルを配列に代入
+   $deletes = file('mission_2-5-1_number_rin.txt'); //削除ファイルを配列に代入
+   $number = $data_number_d - 1;
+   $delete = explode("<>",$deletes[0]); //削除の一行目の<>による配列
+   $fpr = fopen('mission_2-5-1_rin.txt','r+');
+   ftruncate($fpr,0);  //ファイルを空にする
+   fseek($fpr,0);
+   for($a = 0;$a < count($contents);$a++){
+    $content = explode("<>",$contents[$a]); //入力ファイルの各行の<>による配列
+    $pwd = $content[4];
+    $pwd_d = $delete[1];
+    if(($data_number_d == $content[0])&&($pwd == $pwd_d)){
+     echo "削除番号は{$data_number_d}。";
+     echo "入力番号は{$content[0]}。";
+     echo "対象番号一致<br>";
+     echo "入力パスワードは{$pwd}。";
+     echo "削除パスワードは{$pwd_d}。";
+     echo "パスワード一致。この行を削除しました。<br>";
+    }else if($data_number_d == $content[0]){
+     echo "パスワード違います。<br>";
+     $b = $contents[$a];
+     fwrite($fpr,$b);
+    }else{ //指定番号以外のところをファイルへ書き込み
+     $b = $contents[$a];
+     fwrite($fpr,$b);
+    }
+   }
+   fclose($fpr);
+  }
+
+  //内容を編集
+ if(!empty($data_number_n)){
+  $editdata = $data_number_n."<>".$editpassword."<>";
+  fwrite($fpnw_n,$editdata);
+  fclose($fpnw_n);
+ }
+ if(!empty($data_number_n) && ($data_name) && ($data_comment)){
+  $dataname = $_POST['name'];
+  $datacomment = $_POST['comment'];
+  $datapassword = $_POST['password'];
+  $date = date('Y/m/d/ H:i:s');
+  $ns = file('mission_2-5-1_number_n_rin.txt');  //入力フォーム編集用ファイル
+  $n = explode("<>",$ns[0]);
+  $pwd_e = $n[1];
+  $file = file('mission_2-5-1_rin.txt');
+  $fpr = fopen('mission_2-5-1_rin.txt','r+');
+  ftruncate($fpr,0);  //ファイルを空にする
+  fseek($fpr,0);
+  for($a = 0;$a < count($file);$a++){
+   $newline = explode("<>",$file[$a]);
+   $newpwd = $newline[4];
+   $nmb = $newline[0];
+   if(($nmb == $n[0])&&($pwd_e == $newpwd)){  //二つの指定番号とパスワードが一致するとき
+    $number = $a + 1;
+    $c = $number."<>".$dataname."<>".$datacomment."<>".$date."<>".$datapassword."<>"."\n";
+    fwrite($fpr,$c);
+   }else{ //指定番号以外のところをファイルへ書き込み
+    $b = $file[$a];
+    fwrite($fpr,$b);
+   }
+  }
+  fclose($fpr);
+ }else{
+   //ファイルへ書きこみ
+  if(!empty($data_name) && ($data_comment)){
+   $num = count($file);
+   $num ++;
+   $arg = $num."<>".$data_name."<>".$data_comment."<>".$date."<>".$data_password."<>"."\n";
+   fwrite($fpa,$arg);
+   fclose($fpa);
+  }
+ }
+ ?>
+<!DOCTYPE html>
+<html>
+<body>
+<!--入力フォーム-->
+ <form method="post" action="mission_2-5-1_rin.php">
+ <input type="text" name="name" value="<?php echo "$name"; ?>"  size="30"><br>
+ <input type="text" name="comment" value="<?php echo "$comment"; ?>" size="30" ><br>
+ <input type="text" name="password" value="<?php echo "$password"; ?>" size="30">
+ <input type="hidden" name="number_e" value="<?php echo "$editnumber"; ?>" size="30">
+ <input type="hidden" name="passwordedit" value="<?php echo "$passworde"; ?>" size="30">
+ <input type="submit" value="送信"><br><br>
+ </form>
+<!--削除フォーム-->
+ <form method="post" action="mission_2-5-1_rin.php">
+ <input type="text" name="number_delete" value="削除対象番号" size="30"><br>
+ <input type="text" name="password_d" value="パスワード" size="30">
+ <input type="submit" value="削除"><br><br>
+ </form>
+<!--編集番号指定フォーム-->
+ <form method="post" action="mission_2-5-1_rin.php">
+ <input type="text" name="number_edit" value="編集対象番号" size="30"><br>
+ <input type="text" name="password_e" value="パスワード" size="30">
+ <input type="submit" value="編集"><br>
+ </form>
+
+<?php
+  $contents = file('mission_2-5-1_rin.txt');
+  foreach($contents as $value){    //ファイル($file)を一行ずつ($value)配列として認識
+   $line = explode("<>",$value);   //一行($value)を<>で区切る($line)
+   $count = count($line) - 1;
+   for($i = 0;$i < $count;$i++){
+    echo $line[$i]." ";
+   }
+   echo "<br />\n";
+  }
+?>
+</body>
+
+
+</html>
---
+++ mission_4.php
@@ -0,0 +1,115 @@
+﻿<?php
+ //sql接続
+ $dsn = 'データベース名';
+ $user_dsn = 'ユーザー名';
+ $password_dsn = 'パスワード';
+ //データベースへ接続
+ try{
+  $pdo = new PDO($dsn,$user_dsn,$password_dsn);
+  $pdo -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+ }catch(PDOException $e){
+  echo $sql."<br>".$e -> getMessage();
+ }
+ echo "<br>";
+ //変数定義
+ $data_name = $_POST['name'];
+ $data_comment = $_POST['comment'];
+ $data_password = $_POST['password'];
+ $data_date = date('Y/m/d H:i:s');
+ $data_number_delete = $_POST['deletenumber']; //削除対象番号
+ $data_password_delete = $_POST['password_delete']; //削除パスワード
+ $data_number_edit = $_POST['editnumber'];  //編集対象番号
+ $data_password_edit = $_POST['password_edit'];  //編集パスワード
+ $data_number_e = $_POST['number_e']; //入力フォームに代入する編集番号
+ $number_e = "";  //入力フォームの編集対象番号欄に出力される
+ //編集する
+ if(!empty($data_number_edit) && ($data_password_edit)){ //編集の欄が空欄でない
+  try{ //同じ番号のパスワードを取得する
+   $select = $pdo -> query("select pwd from mission where number = $data_number_edit");
+   $result_select = $select -> fetch();
+   $password_select = $result_select['pwd'].PHP_EOL;
+   echo "pwd = {$password_select}";
+  }catch(PDOException $e){
+   echo $sql."<br>".$e -> getMessage();
+  }
+  if($password_edit == $password_select){
+   echo "パスワードが一致"."<br>";
+   $number_e = $data_number_edit;
+   echo "編集対象番号は{$number_e}";
+  }
+ }
+ if(!empty($data_number_e)){
+  try{
+  $newdata = "update mission set name = '$data_name', comment = '$data_comment', day = '$data_date', pwd = '$data_password' where number = '$data_number_e'";
+  $result_newdata = $pdo -> query($newdata);
+  }catch(PDOException $e){
+   echo $sql."<br>".$e -> getMessage();
+  }
+ //入力されたデータをデータベースへ挿入
+ }else if(!empty($data_name) && ($data_comment) && ($data_password)){
+   try{
+   $select = 'select * from mission';
+   $stmt = $pdo -> query($select);
+   $stmt -> execute();
+   $number = $stmt ->rowCount();
+   $number ++;
+   $import = $pdo -> prepare("INSERT INTO mission(number,name,comment,day,pwd)VALUES(:number,:name,:comment,:day,:pwd)");
+   $import -> bindParam(':number',$number);
+   $import -> bindParam(':name',$data_name);
+   $import -> bindParam(':comment',$data_comment);
+   $import -> bindParam(':day',$data_date);
+   $import -> bindParam(':pwd',$data_password);
+   $import -> execute();
+  }catch(PDOException $e){
+   echo $sql."<br>".$e -> getMessage();
+  }
+ }
+ if(!empty($data_number_delete)){
+  try{
+   $delete = "delete from mission where number = $data_number_delete";
+   $result_deletedata = $pdo -> query($delete);
+  }catch(PDOException $e){
+   echo $sql."<br>".$e -> getMessage();
+  }
+ }
+  
+?>
+<html>
+<body>
+ <!-- 入力フォーム -->
+ <form method="post" action="mission_4-1_rin.php">
+ <input type="text" name="name" value="名前" size="30"><br>
+ <input type="text" name="comment" value="コメント" size="30"><br>
+ <input type="text" name="password" value="パスワード" size="30">
+ <input type="text" name="number_e" value="<?php echo "$number_e"; ?>" size="30">
+ <input type="submit" value="送信"><br>
+ </form>
+
+ <!-- 削除フォーム -->
+ <form method="post" action="mission_4-1_rin.php">
+ <input type="text" name="deletenumber" value="削除対象番号" size="30"><br>
+ <input type="text" name="password_delete" value="パスワード" size="30">
+ <input type="submit" value="削除"><br>
+ </form>
+
+ <!-- 編集番号指定フォーム -->
+ <form method="post" action="mission_4-1_rin.php">
+ <input type="text" name="editnumber" value="編集対象番号" size="30"><br>
+ <input type="text" name="password_edit" value="パスワード" size="30">
+ <input type="submit" value="編集"><br>
+ </form>
+
+</body>
+<?php
+ $select = 'SELECT * FROM mission';
+ $results_data_content = $pdo -> query($select);
+ foreach($results_data_content as $row_dc){
+  echo $row_dc['number'].',';
+  echo $row_dc['name'].',';
+  echo $row_dc['comment'].',';
+  echo $row_dc['day'].',';
+  echo $row_dc['pwd'].'<br>';
+ }
+?>
+
+</html>
